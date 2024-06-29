ba696e241794670c09f53d7dae0c9dde727cb580
Mohammed Izhan Ali <mohdizhanali@gmail.com>
2024-06-30 04:17:27 +0530

    Create 02.java

---
+++ practice/patterns/02.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        for(int i = 0; i<5; i++){
+                     for(int j = 0; j<=i; j++){
+                     System.out.print(i+1);
+                     }
+            System.out.println("");
+        }
+        
+        
+    }
+}
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+                     System.out.print(j+1);
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
